a3665b4d6758942813b96f6271e20e71688e8821
en301 <emilsjostrand1000@gmail.com>
2024-02-02 12:29:59 +0100

    Add project files.

---
+++ uppgift7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uppgift_7_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nummerLista = new List<int>();
+            nummerLista.Add(4);
+            nummerLista.Add(5);
+
+            Max(nummerLista);
+            Console.ReadKey();
+        }
+        static void Max(List<int> nummerlista)
+        {
+            nummerlista.Sort();
+            nummerlista.RemoveRange(0, nummerlista.Count-1);
+            foreach (int nummer in nummerlista)
+            {
+                Console.WriteLine(nummer);
+            }
+        }
+    }
+}
